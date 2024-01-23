0c3121234cda0596efb154d55904c925d715530f
divyatambat <tambat.divya06@gmail.com>
2024-01-23 17:47:05 +0530

    Golang Assignment 6-1 uploaded

---
+++ Asg0.6-1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func accessSlice(slice []int, index int) {
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("Internal error: %d\n", err)
+		}
+	}()
+
+	value := slice[index]
+	fmt.Printf("Item: %d, Value: %d\n", index, value)
+}
+
+func main() {
+	slice := []int{10, 20, 30, 40, 50}
+	var index int
+	fmt.Print("Input: ")
+	fmt.Scanln(&index)
+
+	accessSlice(slice, index)
+}
