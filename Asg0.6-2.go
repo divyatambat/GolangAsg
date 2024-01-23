fff1145a6f1035173df2b320a417e0986fcb97ba
divyatambat <tambat.divya06@gmail.com>
2024-01-23 17:48:08 +0530

    Golang Assignment 6-2 uploaded

---
+++ Asg0.6-2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func accessSlice(slice []int, index int) (int, error) {
+
+	if index < 0 || index >= len(slice) {
+		return 0, errors.New("Length of the slice should be more than index!")
+	}
+	return slice[index], nil
+}
+
+func main() {
+	slice := []int{10, 20, 30, 40, 50}
+	var index int
+	fmt.Print("Input: ")
+	fmt.Scanln(&index)
+
+	value, err := accessSlice(slice, index)
+	if err != nil {
+		fmt.Printf("Internal error: %d\n", err)
+	} else {
+		fmt.Printf("Item: %d, Value: %d\n", index, value)
+	}
+
+}
