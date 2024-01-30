ed0b901b65f9d4ca8da0fd72561e16daef11491c
divyatambat <tambat.divya06@gmail.com>
2024-01-30 12:46:06 +0530

    Assignment 8 updated#2

--- Asg0.8.go
+++ Asg0.8.go
@@ -5,29 +5,29 @@ import (
 	"sync"
 )
 
-func isEven(n int) bool {
-	return n%2 == 0
+func isEven(n int, m *sync.Mutex) bool {
+	m.Lock()
+	result := n%2 == 0
+	m.Unlock()
+	return result
 }
 
 func main() {
 	n := 0
 	var wg sync.WaitGroup
 	var mu sync.Mutex
-	
-	mu.Lock()
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()	
-		defer mu.Unlock()
-		nIsEven := isEven(n)
-		
+		nIsEven := isEven(n, &mu)
 		if nIsEven {
 			fmt.Println(n, " is even")
 			return
 		}
 		fmt.Println(n, "is odd")
 	}()
-	
+	//wg.Wait()
 	wg.Add(1)
 	go func() {
 		defer wg.Done()	
